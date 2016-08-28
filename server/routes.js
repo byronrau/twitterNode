37d937da87a5c5d2c55dbbc6cc02456798a68076
Byron Rau <byronrau@gmail.com>
2016-08-27 22:23:24 -0700

    fixing deploy

--- server/routes.js
+++ server/routes.js
@@ -80,5 +80,7 @@ module.exports =  function(app, express, io) {
     });
   });
 
-  app.post('/fb', controller.fb);
+  app.post('/fb', function(req,res,next){
+    controller.fb(req,res, graph);
+  });
 }
