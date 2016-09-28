175f4a641bea559d01a1b0dcb5e66af1562c7c38
Byron Rau <byronrau@gmail.com>
2016-09-28 09:36:36 -0700

    lowering the post count to 25

--- server/controller.js
+++ server/controller.js
@@ -190,7 +190,7 @@ module.exports = {
     var getPosts = function(err, result) {
       count++
       //request to fb api takes longer than 30 secs which times out on free Heroku for count > 20
-      if(result.paging && result.paging.previous && count<15) {
+      if(result.paging && result.paging.previous && count<10) {
         // console.log('calling me', result.paging.previous)
         graph.get(result.paging.next, function(err, resultult){
           // console.log('inside recursive grah.get', result);
